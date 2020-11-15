56cb614881b8efdefe87e6e0b13a9c117ec4a758
fahim-ahmed-7861 <fahimahmedshojib109@gmail.com>
2020-11-15 22:43:13 +0600

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [toggle, setToggle] = useState(false);
+
+  let greeting, food, connection;
+
+
+
+  if (toggle) {
+
+    greeting = <p>welcome my friend</p>
+    food = <p>I will buy food for you</p>
+    connection = <p>Let's join my facebook</p>
+
+
+  }
+  else {
+    greeting = <p>Who hell are you ?</p>
+    food = <p>Lets eat his his whose whose ?</p>
+    connection = <p>i don't add strangers</p>
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>is Familiar : {toggle ? 'False' : 'True'}</h1>
+
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+
+      <h1>Greetings</h1>
+
+      {greeting}
+      <h1>Food</h1>
+
+      {food}
+
+      <h1>Connection</h1>
+
+      {connection}
     </div>
   );
 }
